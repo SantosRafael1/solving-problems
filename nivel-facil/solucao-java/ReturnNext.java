a75cb87c4cfde5e1a391e90ac07f8f104ee2ca3a
Rafael Santos <contactrafaelsantos@gmail.com>
2022-05-31 20:30:24 -0300

    Programa para retornar o próximo número - solução java

---
+++ nivel-facil/solucao-java/ReturnNext.java
@@ -0,0 +1,9 @@
+public class ReturnNext {
+    public static void main(String[] args) {
+        System.out.println(returnNextNumber(3));
+    }
+
+    public static int returnNextNumber(int number) {
+        return number + 1;
+    }
+}
