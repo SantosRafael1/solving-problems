110d5aa114e2c8831903f10deb7f952a9b920fe5
Rafael Santos <contactrafaelsantos@gmail.com>
2022-05-30 21:18:13 -0300

    Algoritmo para somar dois números

---
+++ nivel-facil/solucao-java/ReturnSum.java
@@ -0,0 +1,10 @@
+
+public class ReturnSum {
+    public static void main(String[] args) {
+        System.out.println(returnSum(5, 10));
+    }
+
+    public static int returnSum(int n1, int n2) {
+        return n1 + n2;
+    }
+}
