9ab99277c87ce0948aee2f8645a634ce8d16c6d0
Rafael Santos <contactrafaelsantos@gmail.com>
2022-06-03 18:23:51 -0300

    Calcular área de um triângulo - java

---
+++ nivel-facil/solucao-java/AreaOfTriangle.java
@@ -0,0 +1,10 @@
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        System.out.println(areaOfTriangle(10, 10));
+    }
+
+    public static double areaOfTriangle(double base, double height) {
+        return (base * height) / 2;
+    }
+}
