48634fe2b89fd1bbae39aa96f92d8587705f5ea6
Rafael Santos <contactrafaelsantos@gmail.com>
2022-06-05 15:47:19 -0300

    Converter horas em segundos - java

---
+++ nivel-facil/solucao-java/HoursIntoSeconds.java
@@ -0,0 +1,10 @@
+
+public class HoursIntoSeconds {
+    public static void main(String[] args) {
+        System.out.println(convertHoursIntoSeconds(10));
+    }
+
+    public static int convertHoursIntoSeconds(int hours) {
+        return hours * 3600;
+    }
+}
