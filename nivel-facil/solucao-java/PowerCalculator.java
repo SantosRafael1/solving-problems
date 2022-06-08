5e9b786a5f511ff09c5911a2f156073b0a6d53c6
Rafael Santos <contactrafaelsantos@gmail.com>
2022-06-08 19:53:25 -0300

    Calculadora de energia - java

---
+++ nivel-facil/solucao-java/PowerCalculator.java
@@ -0,0 +1,10 @@
+
+public class PowerCalculator {
+    public static void main(String[] args) {
+        System.out.println(powerCalculator(110, 3));
+    }
+
+    public static double powerCalculator(double voltage, double current) {
+        return voltage * current; 
+    }
+}
