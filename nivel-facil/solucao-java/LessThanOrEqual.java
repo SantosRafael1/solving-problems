1e532b57a56aeef96af2c822f910e8ac3bf0f696
Rafael Santos <contactrafaelsantos@gmail.com>
2022-06-12 16:00:24 -0300

    Verificar se um valor é menor ou igual a zero - java

---
+++ nivel-facil/solucao-java/LessThanOrEqual.java
@@ -0,0 +1,11 @@
+
+public class LessThanOrEqual {
+    public static void main(String[] args) {
+        System.out.println(lessThanOrEqualTo(1));
+    }
+
+    public static boolean lessThanOrEqualTo(int number) {
+        boolean result = number <= 0;
+        return result;
+    }
+}
