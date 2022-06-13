d3f4a6c2037a5991a0c850715e035bbdaaa58298
Rafael Santos <contactrafaelsantos@gmail.com>
2022-06-13 18:55:37 -0300

    converter idade em dias - java

---
+++ nivel-facil/solucao-java/ConvertAgeToDays.java
@@ -0,0 +1,10 @@
+
+public class ConvertAgeToDays {
+    public static void main(String[] args) {
+        System.out.println(calcAge(20));
+    }
+
+    public static int calcAge(int age) {
+        return age * 365;
+    }
+}
